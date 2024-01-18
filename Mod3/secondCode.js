ff4d6686bfd0623a8e6321bc1b88c826e38880a3
higorrofino <higorrofino1@gmail.com>
2024-01-18 17:03:40 -0300

    leaning about variables (cont & let)

--- Mod3/secondCode.js
+++ Mod3/secondCode.js
@@ -1,5 +1,12 @@
-console.log("Hello World, my name is Higor");
-console.log("Hello World, my name is Higor");
-console.log("Hello World, my name is Higor");
-console.log("Hello World, my name is Higor");
-console.log("Hello World, my name is Higor");
+//const screenMessage = "Hello World, my name is Higor"; - immutable variable
+let screenMessage = "Hello World, my name is Higor"; //mutable variable
+
+console.log(screenMessage);
+console.log(screenMessage);
+console.log(screenMessage);
+console.log(screenMessage);
+console.log(screenMessage);
+
+screenMessage = "Updated content in the same variable";
+
+console.log(screenMessage);
