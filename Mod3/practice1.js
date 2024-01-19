7c489c85d70e643c10e06b365fefd1affa3e42de
higorrofino <higorrofino1@gmail.com>
2024-01-18 21:59:27 -0300

    first practice updated

---
+++ Mod3/practice1.js
@@ -0,0 +1,17 @@
+console.log("Exercise 1");
+
+const firstNum = 2148;
+let secondNum;
+console.log(`The value of the first variable is ${firstNum}`);
+
+secondNum = 2;
+console.log(`The value of the second variable is ${secondNum}`);
+
+const sumVal = firstNum + secondNum;
+console.log(`The value of the sum of the variables is ${sumVal}`);
+
+const secondNumExp = secondNum ** 2;
+console.log(`The second value squared is ${secondNumExp}`);
+
+const secondNumDiv = secondNumExp / 3;
+console.log(`The result of the division is ${secondNumDiv}`);
