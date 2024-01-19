cfcadfcfab7676b895d2de80eaa0978d81b313a2
higorrofino <higorrofino1@gmail.com>
2024-01-19 12:14:26 -0300

    module completed

---
+++ Mod3/proveFinal.js
@@ -0,0 +1,38 @@
+// 1
+console.log("Exercise 1");
+
+const listNum = [1, 2, 3, 4, 5];
+console.log(listNum);
+
+console.log(`The value of the second element in my list is ${listNum[1]}`);
+
+//2
+console.log("Exercise 2");
+
+const pokemon = {
+  name: "Blastoize",
+  stamina: 50,
+  def: 7,
+  atk: 15,
+  ability: "Water gun",
+};
+
+console.log(`My pokemon has the ability ${pokemon.ability}`);
+
+//3
+console.log("Exercise 3");
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(`Sum = ${num1 + num2}`);
+console.log(`Subtraction = ${num1 - num2}`);
+console.log(`Division = ${num1 / num2}`);
+console.log(`Multiplication = ${num1 * num2}`);
+
+//4
+console.log("Exercise 4");
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 === num2);
