feba49febe568270a07c44b760220038a0823658
higorrofino <higorrofino1@gmail.com>
2024-07-25 14:44:14 -0300

    com

--- Mod3/valuesUndefinedNull.js
+++
@@ -1,8 +0,0 @@
-// console.log(typeof undefined); - valor padrao do js
-// console.log(typeof null);
-
-let value1 = undefined;
-console.log(typeof value1);
-
-let value2 = null;
-console.log(value2);
